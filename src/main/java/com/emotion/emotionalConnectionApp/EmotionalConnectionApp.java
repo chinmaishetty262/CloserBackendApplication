77491a43daa49ead4bab798db10de94ffe443cb2
Chinmai Shetty <66892324+chinmaishetty262@users.noreply.github.com>
2025-08-19 12:15:19 +0200

    Initial commit

---
+++ src/main/java/com/emotion/emotionalConnectionApp/EmotionalConnectionApp.java
@@ -0,0 +1,13 @@
+package com.emotion.emotionalConnectionApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmotionalConnectionApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmotionalConnectionApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/emotion/emotionalConnectionApp/EmotionalConnectionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emotion.emotionalConnectionApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmotionalConnectionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
